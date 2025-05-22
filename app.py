5757e10a709c0cdd522e60d09f8795c0787dbe14
cometahhalley <naathercc@gmail.com>
2025-05-22 19:16:11 +0300

    Fix for Render: bind to 0.0.0.0 and dynamic port

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 import sqlite3
 from flask import Flask, render_template
 
@@ -31,4 +32,5 @@ def contacto():
     return render_template('contacto.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
