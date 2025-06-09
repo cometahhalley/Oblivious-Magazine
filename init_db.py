0e3864010bbbe9e5988f01fa8142c2bb227d156b
cometahhalley <naathercc@gmail.com>
2025-06-09 23:09:57 +0300

    Local edits to init_db and added LINK TO WEBPAGE.docx

--- init_db.py
+++ init_db.py
@@ -9,12 +9,12 @@ with open('schema.sql') as f:
 # Insertar datos de prueba
 cur = connection.cursor()
 cur.execute(
-    "INSERT INTO issues (issue_number, title, description, cover_image, published_date) VALUES (?, ?, ?, ?, ?)",
+    "INSERT INTO issues (issue_number, title, description, cover_image, published_date, file) VALUES (?, ?, ?, ?, ?, ?)",
     (12, '12. War, guilt and regret', 'Stories exploring conflict and its emotional aftermath.', 'PORTADA12.webp', '2025-05-29', 'issue12.pdf')
 )
 
 cur.execute(
-    "INSERT INTO issues (issue_number, title, description, cover_image, published_date) VALUES (?, ?, ?, ?, ?)",
+    "INSERT INTO issues (issue_number, title, description, cover_image, published_date, file) VALUES (?, ?, ?, ?, ?, ?)",
     (11, '11. Fantasy and science fiction', 'A journey into alternate realities and impossible futures.', 'PORTADA11.webp', '2025-02-21', 'issue11.pdf')
 )
 
